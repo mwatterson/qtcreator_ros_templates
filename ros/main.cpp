763599254fdab10258d4f9131cced3b297d2af4d
Mike <wami@seas.upenn.edu>
2015-05-29 12:12:33 -0700

    updated ros node

--- ros/main.cpp
+++ ros/main.cpp
@@ -1,5 +1,16 @@
 #include <ros/ros.h>
 
+class %CLASS%{
+public:
+    %CLASS%();
+private:
+    ros::NodeHandle nh_;
+
+};
+%CLASS%::%CLASS%() : nh_("~") {
+
+}
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "%NODE%");
